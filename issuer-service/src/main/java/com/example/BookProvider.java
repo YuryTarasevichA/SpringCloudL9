bdf187a87c7176ebcb7bdf7de1dc345f742edf2b
Yura <yura.tarasevichunt@gmail.com>
2024-07-12 00:28:39 +0300

    IssueSerEureka

--- issuer-service/src/main/java/com/example/BookProvider.java
+++ issuer-service/src/main/java/com/example/BookProvider.java
@@ -1,29 +1,46 @@
 package com.example;
 
+import com.netflix.appinfo.InstanceInfo;
+import com.netflix.discovery.EurekaClient;
+import com.netflix.discovery.shared.Application;
 import lombok.Data;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
 
+import java.util.List;
 import java.util.UUID;
+import java.util.concurrent.ThreadLocalRandom;
 
 @Service
 public class BookProvider {
 
     private final WebClient webClient;
+    private final EurekaClient eurekaClient;
 
-    public BookProvider() {
+    public BookProvider(EurekaClient eurekaClient) {
         webClient = WebClient.builder().build();
+        this.eurekaClient = eurekaClient;
     }
 
     public UUID getRandomBookID() {
 
         BookResponse randomBook = webClient.get()
-                .uri("http://localhost:8180/api/book/random")
+                .uri(getBookServiceIp() + "/api/book/random")
                 .retrieve()
                 .bodyToMono(BookResponse.class)
                 .block();
 
         return randomBook.getId();
+    }
+
+    private String getBookServiceIp() {
+        Application application = eurekaClient.getApplication("BOOK-SERVICE");
+        List<InstanceInfo> instances = application.getInstances();
+
+        int randomIndex = ThreadLocalRandom.current().nextInt(instances.size());
+        InstanceInfo randomInstance = instances.get(randomIndex);
+        return "http://" + randomInstance.getIPAddr() + ":" + randomInstance.getPort();
+
     }
 
 
